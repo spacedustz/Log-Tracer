56d9e696f7ddd106401d174274cb54b012626694
KunWoo Shin <cyberspecterr@gmail.com>
2023-05-13 23:58:10 +0900

    Initial Commit

---
+++ src/main/kotlin/com/logtracer/LogTracerApplication.kt
@@ -0,0 +1,11 @@
+package com.logtracer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogTracerApplication
+
+fun main(args: Array<String>) {
+    runApplication<LogTracerApplication>(*args)
+}
